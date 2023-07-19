058a406b9521be0af2931b2c285992cc9de98717
Julien L <julienlenaz96@gmail.com>
2023-07-19 03:05:14 -0400

    Created alignment selecting buttons

--- js/scripts.js
+++ js/scripts.js
@@ -11,7 +11,7 @@ function Character(Name, Gender, Race, Class, Alignment) {
   this.Race = Race,
   this.Class = Class,
   this.Alignment = Alignment,
-  this.details = []
+  this.stats = []
 }
 
 CharacterSheets.prototype.assignId = function() {
@@ -47,8 +47,8 @@ CharacterSheets.prototype.deleteCharacter = function(id) {
   return false;
 }
 
-Character.prototype.addDetails = function(detail) {
-  this.details.push(detail);
+Character.prototype.addStats = function(stat) {
+  this.stats.push(stat);
 }
 
 // UI logic
@@ -64,18 +64,15 @@ function showCharacter(id) {
   $(".race").html(character.Race);
   $(".class").html(character.Class);
   $(".alignment").html(character.Alignment);
-  // var details = "";
-  // character.details.forEach(function(detail) {
-  //   details += "<li>" + detail + "</li>";
+  // var stats = "";
+  // character.stats.forEach(function(stat) {
+  //   stats += "<li>" + stat + "</li>";
   // });
-  // $("#show-details").html(details);
+  // $("#show-stats").html(stats);
   var buttons = $("#buttons");
   buttons.empty();
   buttons.append("<button class='deleteButton' id=" +  + character.id + ">Delete</button>");
 }
-
-// Below uses event delegation, which is the best practice for click handlers. We are attaching click handlers to different dynamic elements. The parent element must exist when the DOM is loaded. For instance, "ul#characters" exists at that time. Then .on() can be used to dynamically attach a listener to a child element, in this case the "li" inside of "ul#characters".
-
 function attachCharacterListeners() {
   var list = $("ul#characters");
   list.on("click", "li", function() {
@@ -101,36 +98,42 @@ function display(book) {
   list.html(records);
 }
 
-// function appendAddress() {
-//   $("#additional-details").append('<div class="new-detail">' +
-//                                  '<div class="form-group">' +
-//                                    '<label for="detail">Address</label>' +
-//                                    '<input type="text" class="form-control detail">' +
-//                                  '</div>');
-// }
+function resetForm() {
+  var form = document.getElementById("new-character"); 
+  form.reset();
+  $("button.alignment-button").removeClass("clicked");
+}
 
 $(document).ready(function() {
   attachCharacterListeners();
-
-  $("form#new-character").submit(function(event) {
+  var clickedAlignment;
+  $("button.alignment-button").click(function(event) {
     event.preventDefault();
+    var alignment = $(this).text();
+    clickedAlignment = alignment;
+    $("button.alignment-button").removeClass("clicked");
+    $(this).addClass("clicked");
+  });
 
+  $("button.submitButton").click(function(event) {
+    event.preventDefault();
     var inputtedName = $("input#new-name").val();
     var inputtedGender = $("input#new-gender").val();
     var inputtedRace = $("input#new-race").val();
     var inputtedClass = $("input#new-class").val();
-    var inputtedAlignment = $("input#new-alignment").val();
+    var inputtedAlignment = clickedAlignment;
     var newCharacter = new Character(inputtedName, inputtedGender, inputtedRace, inputtedClass, inputtedAlignment);
-    $(".new-detail").each(function() {
-      var detail = $(this).find("input.detail").val();
-      newCharacter.addDetails(detail);
+    $(".new-stat").each(function() {
+      var stat = $(this).find("input.stat").val();
+      newCharacter.addStats(stat);
     });
+    resetForm();
     characterSheet.addCharacter(newCharacter);
     display(characterSheet);
-    $("#additional-details").empty();
+    $("#additional-stats").empty();
   });
 
-  // $("#add-detail").click(function() {
-  //   appendAddress();
-  // });
+
+
 });
+
